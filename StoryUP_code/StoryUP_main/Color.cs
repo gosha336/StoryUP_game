526928299ed699f1a1b620aa879c0e314a56d469
gosha336 <aptrazakov.go@gmail.com>
2025-11-17 18:10:04 +0500

    Добавил структуру Color

---
+++ StoryUP_code/StoryUP_main/Color.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoryUP_main
+{
+    public struct Color
+    {
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+
+        public Color(byte r, byte g, byte b)
+        {
+            R = r;
+            G = g;
+            B = b;
+        }
+
+        public override bool Equals([NotNullWhen(true)] object? obj)
+        {
+            return obj is Color other && R == other.R && G == other.G && B == other.B;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(R, G, B);
+        }
+
+        public override string ToString()
+        {
+            return $"{R};{G};{B}";
+        }
+    }
+}
